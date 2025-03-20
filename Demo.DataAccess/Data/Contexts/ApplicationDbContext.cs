909ba8e16d13b63f4437d85a10ebf1ce17d77c10
youssef_74 <youssefessam1293@gmail.com>
2025-03-20 05:25:16 +0200

    Department Module [Entites & Configurations ] and DbContexr

---
+++ Demo.DataAccess/Data/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.DataAccess.Data.Configurations;
+
+namespace Demo.DataAccess.Data.Contexts
+{
+     class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(: "connectionString");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // modelBuilder.ApplyConfiguration<Department>(new DepartmentConfigurations());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+           // modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly)
+        }
+    }
+}
---
+++ Demo.DataAccess/GlobalUsing.cs
@@ -0,0 +1,4 @@
+﻿global using Demo.DataAccess.Models;
+global using Microsoft.EntityFrameworkCore; // TWSL IEntityTypeConfiguration
+global using Microsoft.EntityFrameworkCore.Metadata.Builders; // TWSL (EntityTypeBuilder
+
