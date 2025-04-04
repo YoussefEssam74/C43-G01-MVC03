5603fa33d595aa05af8c496015a67307553e0212
youssef_74 <youssefessam1293@gmail.com>
2025-04-04 18:51:26 +0200

    Controller Create

--- Demo.DataAccess/Repositories/IDepartmentRepository.cs
+++ Demo.DataAccess/Repositories/IDepartmentRepository.cs
@@ -3,6 +3,7 @@ namespace Demo.DataAccess.Repositories
 {
     public interface IDepartmentRepository
     {
+        
         int Add(Department department);
         IEnumerable<Department> GetAll(bool WithTracking = false);
         Department? GetById(int id);
---
+++ Demo.Presentation/Controllers/DepartmentsController.cs
@@ -0,0 +1,61 @@
+﻿using AspNetCoreGeneratedDocument;
+using Demo.BusinessLogic.DataTransferObjects;
+using Demo.BusinessLogic.Services;
+using Demo.DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Presentation.Controllers
+{
+    public class DepartmentsController(IDepartmentService _departmentService,
+        ILogger<DepartmentsController> _logger,
+        IWebHostEnvironment _environment) : Controller
+    {
+        // BaseURL/Department/Index
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var Departments = _departmentService.GetAllDepartments();
+            return View(Departments);
+        }
+
+        #region Create Department
+        [HttpGet]
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(CreatedDepartmentDto departmentDto)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    int Result = _departmentService.AddDepartment(departmentDto);
+                    if (Result > 0)
+                        return RedirectToAction(nameof(Index)); // XXXXXXXX 
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, "Department Can't Be Created");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (_environment.IsDevelopment())
+                    {
+                        // 1. Development => Log Error In Console and Return Same View With Error Message 
+                        ModelState.AddModelError(string.Empty, ex.Message);
+                    }
+                    else
+                    {
+                        // 2. Deployment => Log Error In File | Table in Database And Return Error View 
+                        _logger.LogError(ex.Message);
+                    }
+
+                }
+            }
+            return View(departmentDto);
+
+        }
+
+        #endregion
+    }
+}
