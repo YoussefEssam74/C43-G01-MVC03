c5dbb51a36fb9cd3365f2d158543155c47c87096
youssef_74 <youssefessam1293@gmail.com>
2025-04-06 13:10:14 +0200

    Empoyee Repository & Generic Repository

---
+++ Demo.DataAccess/Repositories/Classes/DepartmentRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Demo.DataAccess.Data.Contexts;
+using Demo.DataAccess.Models.DepartmentModel;
+using Demo.DataAccess.Repositories.Interfaces;
+
+namespace Demo.DataAccess.Repositories.Classes
+{
+    public class DepartmentRepository(ApplicationDbContext dbContext) : GenericRepository<Department>(dbContext), IDepartmentRepository
+    {
+        public Department Youssef()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
