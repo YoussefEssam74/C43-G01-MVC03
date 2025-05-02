f84af0d514378c01801c9344240f9413aa4cd529
youssef_74 <youssefessam1293@gmail.com>
2025-05-02 19:04:13 +0300

    AuthLayout

---
+++ Demo.Presentation/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Presentation.Controllers
+{
+    public class AccountController : Controller
+    {
+        //register
+        public IActionResult Register()=>View();
+        //login
+        //sign out
+
+    }
+}
--- Demo.Presentation/Program.cs
+++ Demo.Presentation/Program.cs
@@ -70,7 +70,7 @@ namespace Demo.Presentation
 
             app.MapControllerRoute(
                 name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+                pattern: "{controller=Account}/{action=Register}/{id?}");
 
             #endregion
             app.Run();
