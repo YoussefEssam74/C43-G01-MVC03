a755cfd68caeb44e4bc1e128b8c0df96aa8ccb48
youssef_74 <youssefessam1293@gmail.com>
2025-05-02 17:27:29 +0300

    Refactor Details Action

--- Demo.BusinessLogic/Profiles/MappingProfiles.cs
+++ Demo.BusinessLogic/Profiles/MappingProfiles.cs
@@ -26,8 +26,11 @@ namespace Demo.BusinessLogic.Profiles
 
                 .ForMember(dest => dest.Gender, Options => Options.MapFrom(src => src.Gender))
                 .ForMember(dest => dest.EmployeeType, options => options.MapFrom(src => src.EmployeeType))
-                .ForMember(dest => dest.HiringDate , options => options.MapFrom( src => DateOnly.FromDateTime(src.HiringDate)))
-                .ForMember(dest => dest.Department, options => options.MapFrom(src => src.Department != null ? src.Department.Name : null));
+                .ForMember(dest => dest.HiringDate, options => options.MapFrom(src => DateOnly.FromDateTime(src.HiringDate)))
+                .ForMember(dest => dest.Department, options => options.MapFrom(src => src.Department != null ? src.Department.Name : null))
+                .ForMember(dest => dest.ImageName, options => options.MapFrom(src => src.Images));
+
+
 
 
             CreateMap<CreatedEmployeeDto, Employee>()
